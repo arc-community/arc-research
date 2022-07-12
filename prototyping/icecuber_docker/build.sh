c0329ddbf1bc05e89c2d833dcdf02bea7f44c3a7
Andreas Koepf <andreas.koepf@xamla.com>
2022-07-12 12:35:20 +0200

    Add simple docker file to evaluate icecuber solution

---
+++ prototyping/icecuber_docker/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t icecuber_solution .
---
+++ prototyping/icecuber_docker/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo inside container run:
+echo $ python3 safe_run.py
+docker run -it --name icecuber_solution icecuber_solution
